2e8bd699209902d37bae2b35f302e6a630caef37
Rite Hook <48453311+ritehook@users.noreply.github.com>
2025-12-12 19:29:06 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-`tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -9,4 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
-`
+
